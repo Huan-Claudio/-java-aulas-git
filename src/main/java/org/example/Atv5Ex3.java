7a0792209482bfc451159e4b556095c55539d744
Huan-Claudio <huanclaudio1610@gmail.com>
2024-08-30 09:35:22 -0300

    projetos

--- src/main/java/org/example/Atv5Ex3.java
+++ src/main/java/org/example/Atv5Ex3.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Atv5Ex3 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner ler = new Scanner(System.in);
 
         double A, B, C;
 
         System.out.print("\nInforme o valor de A: ");
-        A = scanner.nextDouble();
+        A = ler.nextDouble();
         System.out.print("\nInforme o valor de B: ");
-        B = scanner.nextDouble();
+        B = ler.nextDouble();
 
         C = calculaAB(A, B);
         System.out.printf("\nO resultado absoluto é: %.2f\n", C);
 
-        scanner.close();
+        ler.close();
     }
 
     public static double calculaAB(double X, double Y) {
